fa886f155920ae868416daf45d1a95dc638d6ce4
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-11-08 12:00:21 -0300

    003

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/main', [MainController::class, 'index']);
+Route::get('/main/{value}', [MainController::class, 'index']);
