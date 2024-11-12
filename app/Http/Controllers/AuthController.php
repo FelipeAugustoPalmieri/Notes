2f3652231bf4616b68493543aa22aed1456c4411
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-11-11 21:04:13 -0300

    019

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,6 +65,7 @@ class AuthController extends Controller
 
     public function logout()
     {
-        echo 'logout';
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
